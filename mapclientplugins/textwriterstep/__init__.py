02b7f76bd9b54c096b58066ff7b95368c26a4127
TimmyNic <tim.nicolas@outlook.com>
2022-11-28 17:03:32 +1300

    Add __stepname__ attribute to plugin __init__.
    This attribute is used by the MAP-Client to keep track of all the currently installed plugins, without it a number of MAP-Client plugin manager methods do not function correctly.

--- mapclientplugins/textwriterstep/__init__.py
+++ mapclientplugins/textwriterstep/__init__.py
@@ -4,6 +4,7 @@ MAP Client Plugin
 '''
 __version__ = '0.1.0'
 __author__ = 'Li Jiun Chen'
+__stepname__ = 'TextWriter'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.textwriterstep import step
